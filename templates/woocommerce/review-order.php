cd9e53d77dbf80d24f8eec4bcceb7a09feb1f5c0
panteliic <pantelicnikolaa@gmail.com>
2025-07-30 16:58:54 +0200

    checkout i cart

--- templates/woocommerce/review-order.php
+++ templates/woocommerce/review-order.php
@@ -1,9 +1,8 @@
 <?php
 defined( 'ABSPATH' ) || exit;
 
-// wc_get_template( 'checkout/payment.php', array( 'checkout' => WC()->checkout() ) );
+wc_get_template( 'checkout/payment.php', array( 'checkout' => WC()->checkout() ) );
 
 
 
 ?>
-<div class="woocommerce-checkout-review-order-table" style="display:none"></div>
